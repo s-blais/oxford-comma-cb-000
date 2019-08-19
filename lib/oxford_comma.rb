4c77a5171a7ea9c7559dd84e7e8d6819890091ef
Stephen Blais <stephen@s-blais.com>
2019-08-19 23:24:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length >=3
+    string_output = ("and #{array.pop}")
+    array.reverse.each do |item|
+      string_output.prepend ("#{item}, ")
+    end
+  elsif array.length == 2
+    string_output = "#{array[0]} and #{array[1]}"
+  else string_output = "#{array[0]}"
+  end
+  string_output
 end
