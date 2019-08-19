9a163f4cc01124bc64a5808291a9999405cbac5a
Stephen Blais <stephen@s-blais.com>
2019-08-19 23:01:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,13 @@
 def oxford_comma(array)
+  string_output = ""
   if array.length >=3
     item = array.pop
-    string_output.unshift ("and #{item}")
+    string_output = ("and #{item}")
+    array.each do |item|
+      string_output.prepend ("#{item}, ")
+  elsif array.length == 2
+    string_output = "#{array[1]} and #{array[2]}"
+  else string_output = "#{array[1]}"
   end
+  string_output
 end
